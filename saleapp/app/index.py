15c4956e6e8ebd68e389963b42752e507426f568
phanthanhtrinh0000@gmail.com <phanthanhtrinh0000@gmail.com>
2024-11-07 23:11:58 +0700

    init-project

--- saleapp/app/index.py
+++ saleapp/app/index.py
@@ -1,13 +1,16 @@
-from flask import render_template
+from flask import render_template, request
 import dao
 from app import app
 
 
 @app.route("/")
 def index():
-    cates= dao.load_categories()
-    pros=dao.load_products()
-    return render_template("index.html",categories=cates,products=pros)
+    cates = dao.load_categories()
+
+    cate_id = request.args.get('category_id')
+    kw = request.args.get('kw')
+    pros = dao.load_products(cate_id=cate_id, kw=kw)
+    return render_template("index.html", categories=cates, products=pros)
 
 
 if __name__ == '__main__':
